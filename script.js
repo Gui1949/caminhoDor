ae38f049bba63e73c04d69e10d3a5b71215c0f89
Hugomathe <matheushugo59@gmail.com>
2021-07-06 17:52:36 -0300

    veagora

--- script.js
+++ script.js
@@ -422,31 +422,6 @@ show_respostas = (elemento) => {
 
 // Fim Ações
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 function modalf() {
 
 document.getElementById("modalativo").style.animationName ="bmodal";}
@@ -610,7 +585,7 @@ function modala(v) {
       document.getElementById("modalativo").innerHTML += "<div id='femodal' onclick='modalf ()'><p>OK</p></div>";
     break;
     case 20:
-      
+
       $('#modalativo').empty();
       document.getElementById("modalativo").style.animationName = "amodal";
       document.getElementById("modalativo").style.borderColor = "#01ea77"; 
